b0e47812ad796192cce17f508b3a235d601c0451
Hexilee <i@hexilee.me>
2019-11-10 16:26:51 +0800

    start

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![no_std]
+#![feature(lang_items, start)]
+
+//extern crate libc;
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn handle_panic(_: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn abort() {
+    panic!("abort")
+}
+
+#[start]
+fn start(_argc: isize, _args: *const *const u8) -> isize {
+    0
+}
